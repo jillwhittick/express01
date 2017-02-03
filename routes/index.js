52d1680cb50cead1e7195ecb33a52b8e01a2c3f5
Jill Whittick <jill_whittick@yahoo.ca>
2017-02-03 22:37:34 +0000

    Updated index.jade and created hello.jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hello' });
+});
+
+router.post('/', function (req, res) {
+    console.log(req.body.name);
+    res.render('hello', {name: req.body.name });
 });
 
 module.exports = router;
